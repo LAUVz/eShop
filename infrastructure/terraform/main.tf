90e978dbf4828be77296acb891778ca05cfd4610
Henrijs Kons <henrijskons@gmail.com>
2026-01-16 18:07:58 +0100

    Bump to see workflow running properly.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -163,7 +163,7 @@ module "ecs" {
   ecs_security_group_id  = module.security_groups.ecs_security_group_id
   alb_target_group_arns  = module.alb.target_group_arns
   alb_dns_name           = module.alb.alb_dns_name
-  app_url                = "https://eshop.henrijskons.eu"  # Custom domain with HTTPS
+  app_url                = ""  # Use ALB DNS (HTTPS will be added after certificate setup)
   services               = local.services
 
   # Dependencies
